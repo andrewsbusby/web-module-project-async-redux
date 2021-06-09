9ae7d4a869fc02a863651e91f7e4ba58c6ad7de4
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-06-09 15:13:43 -0600

    new components and spartan file

---
+++ app/src/components/spartan.js
@@ -0,0 +1,38 @@
+import React, {useEffect} from 'react';
+import { connect } from 'react-redux';
+
+import { fetchFail, fetchStart, fetchSuccess, getSpartan } from './../actions/index';
+
+const Spartan = (props) => {
+    const { spartan, isFetching, error } = props;
+    useEffect(() => {
+        props.dispatch(getSpartan());
+    }, []);
+    const handleClick = () => {
+        props.dispatch(getSpartan());
+    }
+    if (error) {
+        return <h2>We got an error: {error}</h2>
+    }
+    if (isFetching) {
+        return <h2>Spartan incoming!!</h2>
+    }
+
+    return (
+        <> 
+            <div>
+                <h2>Say hello to: {gamertag} </h2>
+            </div>
+        </>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        spartan: state.spartan,
+        isFetching: state.isFetching,
+        error: state.error
+    };
+}
+
+export default connect(mapStateToProps) (Spartan);
